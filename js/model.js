d77ca91498467a342358cb680716963ab5990af3
grapefruit224 <prikey7@gmail.com>
2024-05-28 00:59:04 +0900

    fixed modal btn

--- js/model.js
+++ js/model.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById('myModal');
     const btn = document.querySelector('.subs_btn');
     const span = document.querySelector('.close');
+    const close = document.querySelector('.close-btn');
 
     // 구독 버튼 클릭 이벤트 추가
     btn.addEventListener('click', (event) => {
@@ -11,8 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // 모달 닫기 버튼 클릭 이벤트 추가
-    span.addEventListener('click', () => {
+    close.addEventListener('click', () => {
         modal.style.display = 'none';
+        console.log('wow!')
     });
 
     // 모달 외부 클릭 시 닫기
