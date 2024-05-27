b8449cd16ef526886bb665c79733ee808a248eea
grapefruit224 <prikey7@gmail.com>
2024-05-27 18:28:17 +0900

    Add modal

---
+++ js/model.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // 모달 요소 가져오기
+    const modal = document.getElementById('myModal');
+    const btn = document.querySelector('.subs_btn');
+    const span = document.querySelector('.close');
+
+    // 구독 버튼 클릭 이벤트 추가
+    btn.addEventListener('click', (event) => {
+        modal.style.display = 'block';
+        console.log(event);
+    });
+
+    // 모달 닫기 버튼 클릭 이벤트 추가
+    span.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // 모달 외부 클릭 시 닫기
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
